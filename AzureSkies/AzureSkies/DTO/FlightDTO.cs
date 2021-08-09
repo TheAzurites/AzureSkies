a5e0a70e796b21d3b4404993b878426dbdf16e5c
Benjamin <loco.queens@gmail.com>
2021-08-09 16:00:19 -0700

    added models,interface,service,sms fucntionality

---
+++ AzureSkies/AzureSkies/DTO/FlightDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureSkies.DTO
+{
+    public class FlightDTO
+    {
+        // Flight {FlightNumber}
+        public string FlightNumber { get; set; }
+        // with {Airline}
+        public string Airline { get; set; }
+        // on {Date},
+        public string Date { get; set; }
+        // current status is: {FlightStatus}
+        public string FlightStatus { get; set; }
+    }
+}
